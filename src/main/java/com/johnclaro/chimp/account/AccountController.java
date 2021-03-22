46a8e24d621b935d0c7950eec73c688ae3f3f9af
John Claro <jkrclaro@gmail.com>
2021-03-22 21:43:19 +0000

    Return JSON for /register

--- src/main/java/com/johnclaro/chimp/account/AccountController.java
+++ src/main/java/com/johnclaro/chimp/account/AccountController.java
@@ -3,14 +3,18 @@ package com.johnclaro.chimp.account;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-
+import java.util.Map;
+import java.util.HashMap;
 
 @RestController
 public class AccountController {
 
     @GetMapping({"/register"})
-    public String register(Account account) {
+    public Map<String, String> register(Account account) {
         Account newAccount = new Account("john", "moo");
-        return newAccount.getUsername();
+        Map<String, String> response = new HashMap<>();
+        response.put("usernames", newAccount.getUsername());
+        response.put("password", newAccount.getPassword());
+        return response;
     }
 }
