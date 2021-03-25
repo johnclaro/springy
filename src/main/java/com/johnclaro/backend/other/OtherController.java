b840cd8b5cd142d218035ca829e828f51d102927
John Claro <jkrclaro@gmail.com>
2021-03-25 01:32:39 +0000

    Create health endpoint that returns 200 after a GET request

--- src/main/java/com/johnclaro/backend/other/OtherController.java
+++ src/main/java/com/johnclaro/backend/other/OtherController.java
@@ -3,6 +3,7 @@ package com.johnclaro.backend.other;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.http.ResponseEntity;
 
@@ -18,5 +19,11 @@ public class OtherController {
         HttpHeaders httpHeaders = new HttpHeaders();
         httpHeaders.setLocation(frontend);
         return new ResponseEntity<>(httpHeaders, HttpStatus.SEE_OTHER);
+    }
+
+    @GetMapping({"/health"})
+    @ResponseStatus(HttpStatus.OK)
+    public String health() {
+        return ":)";
     }
 }
